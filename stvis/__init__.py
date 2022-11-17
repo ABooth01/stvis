5f70e162045fbc30259e92c70c289edab3f6875d
ABooth01 <102238996+ABooth01@users.noreply.github.com>
2022-11-17 08:46:45 +0000

    Update __init__.py
    Attempted to add a random number to the generated html file that links it to the current session. In doing so allows multiple users to generate separate graphs.

--- stvis/__init__.py
+++ stvis/__init__.py
@@ -1,15 +1,15 @@
 import streamlit.components.v1 as components
-#def pv_static(fig,name='graph'):
-#  h1=fig.height
-#  h1=int(h1.replace('px',''))
-#  w1=fig.width
-#  w1=int(w1.replace('px',''))
-#  fig.show(name+'.html')
-#  return components.html(
-#    fig.html,height=h1+30, width=w1+30
-#       )
+import streamlit as st
+import random
 
-def pv_static(fig,name='graph'):
+num = random.randint(0,99999)
+
+if 'key' not in st.session_state:
+    st.session_state['key'] = num
+
+num1 = st.session_state['key']
+
+def pv_static(fig,name=num1+'graph'):
     h1=fig.height
     h1=int(h1.replace('px',''))
     w1=fig.width
